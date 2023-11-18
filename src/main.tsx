608ba7564909bae2fc6d042b49986bfde923e6fb
MarcosG119 <86250436+MarcosG119@users.noreply.github.com>
2023-11-17 16:59:57 -0800

    added routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage.tsx';
+import WorkExperience from './pages/work-experience/WorkExperience.tsx'
+import Resume from './pages/resume/Resume.tsx';
+import Hobbies from './pages/hobbies/Hobbies.tsx';
+import AboutMe from './pages/about-me/AboutMe.tsx';
+import ContactMe from './pages/contact-me/ContactMe.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Homepage />
+            },
+            {
+                path: "/work-experience/",
+                element: <WorkExperience />
+            },
+            {
+                path: "/resume/",
+                element: <Resume />
+            },
+            {
+                path: "/hobbies/",
+                element: <Hobbies />
+            },
+            {
+                path: "/about-me/",
+                element: <AboutMe />
+            },
+            {
+                path: "/contact/",
+                element: <ContactMe />
+            }
+        ]
+
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Homepage />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
